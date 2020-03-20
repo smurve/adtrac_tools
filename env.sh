bd47bbef8a1032feb75ffb1fc9ee4c625fd92dbd
Wolfgang Giersche <wgiersche@gmail.com>
2020-03-20 07:32:12 +0100

    new tools

--- env.sh
+++ env.sh
@@ -1,7 +1,12 @@
 # source this from within your python repo folder
-export DJANGO_SETTINGS_MODULE=brokerage.settings.unittest
+s=$1
+[ -z "$1" ] && s=unittest
+export DJANGO_SETTINGS_MODULE=brokerage.settings.$s
 export PROJECT_PATH=$PWD/..
 export PGPASSWORD=postgres
 export PATH=$PATH:$PROJECT_PATH/tools
 export PYTHONPATH=$PWD:$PYTHONPATH
 export GOOGLE_APPLICATION_CREDENTIALS=$PROJECT_PATH/secrets/GCS_Object_Access.json
+export SENDGRID_API_KEY=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
+  
+  
